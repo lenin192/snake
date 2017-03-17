246007e664e138a6f22a40f8d09fb26d55743fa6
lenin1 <andrew192@mail.ru>
2017-03-17 12:17:53 +0300

    lesson 2

--- Program.cs
+++ Program.cs
@@ -6,8 +6,27 @@ namespace snake
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
-			Console.ReadLine();
+		int x1 = 1;
+		int y1 = 2;
+		char sym1 = '*';
+        Console.SetCursorPosition(x1, y1);
+        Console.Write(sym1);
+
+        int x2 = 3;
+        int y2 = 4;
+        char sym2 = '@';
+
+        Console.SetCursorPosition(x2, y2);
+        Console.Write(sym2);
+
+        int x3 = 5;
+        int y3 = 6;
+        char sym3 = '#';
+
+        Console.SetCursorPosition(x3, y3);
+        Console.Write(sym3);
+
+        Console.ReadLine();
 		}
 	}
 }
